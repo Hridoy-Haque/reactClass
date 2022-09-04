4727edff77a0816742fd82eb10ad5f2b1d7973ae
Hridoy <iamnirob334@gmail.com>
2022-09-04 12:40:37 +0600

    add nested mapping code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React from "react";
-import Card from "./components/Card";
-import Data from "./data.json";
+
+const users = [
+  {
+    fullName: "Hridoy Haque",
+    age: 21,
+    phones: [{ home: "0183627782" }, { office: "01836278789" }],
+  },
+  {
+    fullName: "Anik Ahmed",
+    age: 15,
+    phones: [{ home: "0183627766" }, { office: "01836278765" }],
+  },
+  {
+    fullName: "Nirob Ahmed",
+    age: 32,
+    phones: [{ home: "01836277890" }, { office: "0183627879" }],
+  },
+];
 
 export default function App() {
-  // let items = [];
-  // for (let x = 0; x < Data.length; x++) {
-  //   items.push(<Card todoTitle={Data[x].title} todoDesc={Data[x].desc} />);
-  // }
-  // items = Data.map((item) => {
-  //   console.log(item);
-  //   return <Card todoTitle={item.title} todoDesc={item.desc} />;
-  // });
   return (
     <div>
-      <h1 className="headingStyle">Todo App</h1>
-      {Data.map((item, key) => (
-        <Card key={key} todoTitle={item.title} todoDesc={item.desc} />
+      <h1>Nested List</h1>
+      {users.map((user, index) => (
+        <article key={index}>
+          <h3>{user.fullName}</h3>
+          <p>{user.age}</p>
+          {user.phones.map((phone, index) => (
+            <div key={index}>
+              <p>{phone.home}</p>
+              <p>{phone.office}</p>
+            </div>
+          ))}
+        </article>
       ))}
     </div>
   );
